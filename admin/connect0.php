d2f041ee0db6dbb5733b6ab1e1240ae098c68e28
Aasma1011 <asmashaikh1110@gmail.com>
2019-12-13 00:11:27 +0530

    updated

---
+++ admin/connect0.php
@@ -0,0 +1,10 @@
+<?php 
+$con = mysql_connect("localhost","root","","dotcode");
+
+if (!$con)
+{
+  die();
+}
+
+//mysql_select_db("dotcode",$con);
+?>
---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php 
+$con = mysqli_connect("localhost","root","","dotcode");
+
+if (!$con)
+{
+  die();
+}
+
+//mysql_select_db("dotcode",$con);
+?>
